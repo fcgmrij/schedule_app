cfba17937dfd644fab7d040356f7cf150c21e5e6
fcgmrij <fcgmrij020210@gmail.com>
2026-07-08 18:27:56 +0900

    Fix Vercel function exports

--- backend/api/[...route].ts
+++ backend/api/[...route].ts
@@ -1,4 +1,10 @@
 import { handle } from "hono/vercel";
 import app from "../src/index.js";
 
-export default handle(app);
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
+export const DELETE = handler;
+export const OPTIONS = handler;
